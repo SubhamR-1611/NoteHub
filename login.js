4e968028624123dd1c377e5550ba7f3995b50e0c
PraveenKdash <praveenpkd2004@gmail.com>
2026-06-26 17:08:34 +0530

    Added login JavaScript

---
+++ login.js
@@ -0,0 +1,24 @@
+const form = document.querySelector("form");
+const email = document.querySelector(".email");
+const password = document.querySelector('input[type="password"]');
+
+form.addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    if (email.value.trim() === "" || password.value.trim() === "") {
+        alert("Please fill all the fields.");
+    }
+    else if (!email.value.includes("@") || !email.value.includes(".")) {
+        alert("Please enter a valid email address.");
+    }
+    else if (password.value.length < 6) {
+        alert("Password must be at least 6 characters long.");
+    }
+    else {
+        alert("Login Successful!");
+        form.reset();
+         window.location.href = "dashboard.html";
+    }
+
+});
